eb77233bb5293fb4d34a8f09981c6612c79ef23d
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-01-17 13:55:49 -0800

    basic structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 import './css/styles.scss';
+
+/*
+howl: 
+intelligence: 0
+strength: 5
+charm: 0
+
+luna: 
+intelligence: 0
+strength: 0
+charm: 5
+
+thorn: 
+intelligence: 5
+strength: 0
+charm: 0
+*/
+
+const createCharacter = function(name, strengthVal = 0, charmVal = 0, intelligenceVal = 0) {
+    const obj = {
+        name: name,
+        strength: strengthVal,
+        charm: charmVal,
+        intelligence: intelligenceVal,
+        levelUp: function(skill) {
+            this[skill] += 1;
+        }
+    };
+    return obj;
+};
+
+
+// const luna = createCharacter("Luna", 0, 5, 0);
+// const thorn = createCharacter("Thorn", 0, 0, 5);
+
+function operator() {
+    document.getElementById("howl").onclick = function() {
+        const howl = createCharacter("Howl", 5, 0, 0);
+        document.getElementById("character").classList.add("hidden");
+        document.getElementById("game").classList.remove("hidden");
+        document.getElementById("user-name").innerText = howl.name;
+        document.getElementById("strength").innerText = howl.strength;
+        document.getElementById("charm").innerText = howl.charm;
+        document.getElementById("intelligence").innerText = howl.intelligence;
+    };
+}
+
+window.onload = () => {
+    operator();
+};
--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,18 @@ module.exports = {
             }
           }
         ]
-      }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
     ]
   }
 };
